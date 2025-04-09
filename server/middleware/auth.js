072921aa9feb866b608d1e4b336046c756994cab
stanysaizi <stanysaizi4@gmail.com>
2025-04-09 15:06:27 +0200

    Add server/middleware/auth.js 1744203997032

---
+++ server/middleware/auth.js
@@ -0,0 +1,39 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config/config');
+const User = require('../models/User');
+
+// Protect routes with JWT
+exports.protect = async (req, res, next) => {
+  let token;
+
+  // Get token from header
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  // Make sure token exists
+  if (!token) {
+    return res.status(401).json({ message: 'Not authorized to access this route' });
+  }
+
+  try {
+    // Verify token
+    const decoded = jwt.verify(token, config.JWT_SECRET);
+    req.user = await User.findById(decoded.user.id).select('-password');
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: 'Not authorized to access this route' });
+  }
+};
+
+// Grant access to specific roles
+exports.authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({
+        message: `User role ${req.user.role} is not authorized to access this route`
+      });
+    }
+    next();
+  };
+};
